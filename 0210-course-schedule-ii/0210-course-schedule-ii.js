6b8fc03df0512ca1c371fd21853e68f91ebe9022
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-07-24 14:49:59 +0900

    Time: 5 ms (95.18%), Space: 59.8 MB (94.38%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    const graph = Array.from({length : numCourses}, () => []); // graph[a] = [b, c, d]
+    const indegree = Array.from({length : numCourses}, () => 0); // graph[a] = 3
+    const q = [];
+    const result = [];
+    let head = 0;
+    let count = 0;
+
+    for(const [a, b] of prerequisites) {
+        graph[b].push(a);
+        indegree[a]++;
+    }
+
+    for(let i = 0; i < numCourses; i++) {
+        if(indegree[i] === 0) q.push(i);
+    }
+
+    while(head < q.length) {
+        const cur = q[head++];
+        result.push(cur);
+        count++;
+        
+        if(indegree[cur] !== 0) return [];
+
+        for(const next of graph[cur]) {
+            indegree[next]--;
+            if(indegree[next] === 0) q.push(next);
+        }
+    }
+
+    return count === numCourses ? result : [];
+}; 
